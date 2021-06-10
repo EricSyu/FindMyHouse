ce8daa6f0c6948335613869377c9fb0c604b281c
WeiJhe <sky820902@gmail.com>
2021-06-10 23:53:02 +0800

    add Models generate from mysql using db first

---
+++ Models/House.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace HouseViewer.Models
+{
+    public partial class House
+    {
+        public string Id { get; set; }
+        public string Type { get; set; }
+        public string Kind { get; set; }
+        public string Shape { get; set; }
+        public string Region { get; set; }
+        public string Section { get; set; }
+        public string Title { get; set; }
+        public string Carport { get; set; }
+        public string Room { get; set; }
+        public string Floor { get; set; }
+        public float Area { get; set; }
+        public int HouseAge { get; set; }
+        public float UnitPrice { get; set; }
+        public int Price { get; set; }
+        public string Link { get; set; }
+        public string Other { get; set; }
+        public string DataFrom { get; set; }
+        public DateTime RecordTime { get; set; }
+        public int Rank { get; set; }
+        public string Comment { get; set; }
+        public bool Discard { get; set; }
+    }
+}
---
+++ Models/search_house_appContext.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace HouseViewer.Models
+{
+    public partial class search_house_appContext : DbContext
+    {
+        public search_house_appContext()
+        {
+        }
+
+        public search_house_appContext(DbContextOptions<search_house_appContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<House> Houses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasCharSet("utf8mb4")
+                .UseCollation("utf8mb4_0900_ai_ci");
+
+            modelBuilder.Entity<House>(entity =>
+            {
+                entity.ToTable("house");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Area).HasColumnName("area");
+
+                entity.Property(e => e.Carport)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .HasColumnName("carport");
+
+                entity.Property(e => e.Comment)
+                    .HasMaxLength(255)
+                    .HasColumnName("comment");
+
+                entity.Property(e => e.DataFrom)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .HasColumnName("data_from");
+
+                entity.Property(e => e.Discard).HasColumnName("discard");
+
+                entity.Property(e => e.Floor)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .HasColumnName("floor");
+
+                entity.Property(e => e.HouseAge).HasColumnName("house_age");
+
+                entity.Property(e => e.Kind)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .HasColumnName("kind");
+
+                entity.Property(e => e.Link)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .HasColumnName("link");
+
+                entity.Property(e => e.Other)
+                    .HasMaxLength(255)
+                    .HasColumnName("other");
+
+                entity.Property(e => e.Price).HasColumnName("price");
+
+                entity.Property(e => e.Rank).HasColumnName("rank");
+
+                entity.Property(e => e.RecordTime)
+                    .HasColumnType("datetime")
+                    .HasColumnName("record_time");
+
+                entity.Property(e => e.Region)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .HasColumnName("region");
+
+                entity.Property(e => e.Room)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .HasColumnName("room");
+
+                entity.Property(e => e.Section)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .HasColumnName("section");
+
+                entity.Property(e => e.Shape)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .HasColumnName("shape");
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .HasColumnName("title");
+
+                entity.Property(e => e.Type)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .HasColumnName("type");
+
+                entity.Property(e => e.UnitPrice).HasColumnName("unit_price");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
