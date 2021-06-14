94c77650d8684f6cd760e48d20039afb043d387a
WeiJhe <sky820902@gmail.com>
2021-06-15 01:43:00 +0800

    add to fetch house data from web api and show it using table of bootstrap

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import {React, useState, useEffect} from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+import { Navbar, Table } from 'react-bootstrap';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // eslint-disable-next-line no-unused-vars
+    const [houses, setHouses] = useState([])
+    
+    useEffect(() => {
+        fetchHouseData();
+    }, []);
+
+    async function fetchHouseData() {
+        let response = await fetch("api/House");
+        let jsonData = await response.json();
+        setHouses(jsonData);
+    }
+
+    return (
+        <div className="App">
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="#">House Viewer</Navbar.Brand>
+        </Navbar>
+
+        <div className="content">
+            <Table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>title</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    houses.map((h, i) => (
+                        <tr key={i}>
+                            <td>{h.id}</td>
+                            <td>{h.title}</td>
+                        </tr>
+                    ))
+                }
+            </tbody>
+            </Table>
+        </div>
+
+        </div>
+    );
 }
 
 export default App;
