b72afc74bb74e66b46d1ff22fa17558723f0e1c1
WeiJhe <sky820902@gmail.com>
2021-06-17 00:51:48 +0800

    replace table to BootstrapTable

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -1,13 +1,24 @@
 import {React, useState, useEffect} from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import './App.css';
 
 import { Navbar, Table } from 'react-bootstrap';
+import BootstrapTable from 'react-bootstrap-table-next';
 
 function App() {
-    // eslint-disable-next-line no-unused-vars
     const [houses, setHouses] = useState([])
+    const columns = [{
+        dataField: 'id',
+        text: 'id'
+      }, {
+        dataField: 'title',
+        text: '標題'
+      }, {
+        dataField: 'price',
+        text: '價格'
+      }];
     
     useEffect(() => {
         fetchHouseData();
@@ -21,30 +32,15 @@ function App() {
 
     return (
         <div className="App">
-        <Navbar bg="dark" variant="dark">
-            <Navbar.Brand href="#">House Viewer</Navbar.Brand>
-        </Navbar>
+            <div className="header">
+                <Navbar bg="dark" variant="dark">
+                    <Navbar.Brand href="#">House Viewer</Navbar.Brand>
+                </Navbar>
+            </div>
 
-        <div className="content">
-            <Table>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>title</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    houses.map((h, i) => (
-                        <tr key={i}>
-                            <td>{h.id}</td>
-                            <td>{h.title}</td>
-                        </tr>
-                    ))
-                }
-            </tbody>
-            </Table>
-        </div>
+            <div className="content">
+                <BootstrapTable keyField='id' data={houses} columns={columns} />
+            </div>
 
         </div>
     );
