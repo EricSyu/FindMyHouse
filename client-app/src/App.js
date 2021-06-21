85560767d07c146f1efb958808ea99b49964bca7
WeiJhe <sky820902@gmail.com>
2021-06-22 00:32:48 +0800

    reorganize layout

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -4,21 +4,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import './App.css';
 
-import { Navbar, Table } from 'react-bootstrap';
+import { Navbar, Container, Row, Col } from 'react-bootstrap';
 import BootstrapTable from 'react-bootstrap-table-next';
 
 function App() {
     const [houses, setHouses] = useState([])
-    const columns = [{
-        dataField: 'id',
-        text: 'id'
-      }, {
-        dataField: 'title',
-        text: '標題'
-      }, {
-        dataField: 'price',
-        text: '價格'
-      }];
+    const columns = [
+        {
+            dataField: 'id',
+            text: 'id'
+        }, 
+        {
+            dataField: 'title',
+            text: '標題'
+        }, 
+        {
+            dataField: 'price',
+            text: '價格'
+        }];
     
     useEffect(() => {
         fetchHouseData();
@@ -34,14 +37,20 @@ function App() {
         <div className="App">
             <div className="header">
                 <Navbar bg="dark" variant="dark">
-                    <Navbar.Brand href="#">House Viewer</Navbar.Brand>
+                    <Container fluid>
+                        <Navbar.Brand href="#home">House Viewer</Navbar.Brand>
+                    </Container>
                 </Navbar>
             </div>
-
-            <div className="content">
-                <BootstrapTable keyField='id' data={houses} columns={columns} />
+            <div className="content p-1">
+                <Container fluid>
+                    <Row>
+                        <Col>
+                            <BootstrapTable keyField='id' data={houses} columns={columns} wrapperClasses="table-responsive"/>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
-
         </div>
     );
 }
