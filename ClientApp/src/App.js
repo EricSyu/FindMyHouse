6f8625e1f9ef41d7059feb3518776b5c7d9c493c
WeiJhe <sky820902@gmail.com>
2021-06-27 00:07:58 +0800

    re-orginize project directory

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -2,7 +2,6 @@
 import { React, useState, useEffect, useMemo } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import './App.css';
 
 import { Navbar, Container, Row, Col } from 'react-bootstrap';
