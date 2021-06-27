40d75c2695fddb5b8f01c1221d1dfe6e6b16b2e3
WeiJhe <sky820902@gmail.com>
2021-06-27 17:26:25 +0800

    add antd Table

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,13 +1,34 @@
-/* eslint-disable react/jsx-key */
 import { React, useState, useEffect, useMemo } from 'react';
+import { Navbar, Container, Row, Col } from 'react-bootstrap';
+import { Table } from 'antd';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'antd/dist/antd.css'
 import './App.css';
-
-import { Navbar, Container, Row, Col } from 'react-bootstrap';
 
 function App() {
   const [houses, setHouses] = useState([])
+  const columns = [
+    { dataIndex: 'type', key: 'type', title: '類型' },
+    { dataIndex: 'shape', key: 'shape', title: '型態' },
+    { dataIndex: 'section', key: 'section', title: '區域' },
+    { dataIndex: 'title', key: 'title', title: '標題' },
+    { dataIndex: 'carport', key: 'carport', title: '車位' },
+    { dataIndex: 'room', key: 'room', title: '格局' },
+    { dataIndex: 'floor', key: 'floor', title: '樓層' },
+    { dataIndex: 'area', key: 'area', title: '坪數' },
+    { dataIndex: 'houseAge', key: 'houseAge', title: '屋齡' },
+    { dataIndex: 'unitPrice', key: 'unitPrice', title: '單價(萬/坪)' },
+    { dataIndex: 'price', key: 'price', title: '總價(萬)' },
+    { dataIndex: 'link', key: 'link', title: '連結' },
+    { dataIndex: 'dataFrom', key: 'dataFrom', title: '資料來源' },
+    { dataIndex: 'recordTime', key: 'recordTime', title: '紀錄時間' },
+    { dataIndex: 'comment', key: 'comment', title: '備註' }
+  ];
+
+  useEffect(() => {
+    fetchHouseData();
+  }, []);
 
   async function fetchHouseData() {
       let response = await fetch("api/House");
@@ -28,6 +49,7 @@ function App() {
         <Container fluid>
           <Row>
             <Col>
+              <Table columns={columns} dataSource={houses} rowKey={house => house.id} scroll={{ x: 'max-content' }} />
             </Col>
           </Row>
         </Container>
